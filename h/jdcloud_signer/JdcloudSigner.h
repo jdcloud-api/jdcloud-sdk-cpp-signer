d0c23a207cf7fd01f5b57c34b72cdd700f5166a9
LI Daobing <lidaobing@gmail.com>
2019-02-28 00:33:26 +0800

    split impl and interface

--- h/jdcloud_signer/JdcloudSigner.h
+++ h/jdcloud_signer/JdcloudSigner.h
@@ -17,15 +17,7 @@
 #pragma once
 
 #include <string>
-#include <map>
-#include <set>
-#include <iostream>
-#include <sstream>
-#include <algorithm>
 #include "jdcloud_signer/Credential.h"
-#include "jdcloud_signer/util/crypto/Sha256.h"
-#include "jdcloud_signer/util/crypto/Sha256HMAC.h"
-#include "jdcloud_signer/util/DateTime.h"
 #include "jdcloud_signer/http/HttpRequest.h"
 
 namespace jdcloud_signer {
@@ -38,25 +30,10 @@ public:
     virtual ~JdcloudSigner();
 
     bool SignRequest(HttpRequest& request) const;
-
 private:
-    bool ShouldSignHeader(const std::string& header) const;
-    std::string GenerateSignature(const Credential& credentials, const std::string& stringToSign, const std::string& simpleDate) const;
-    std::string GenerateSignature(const std::string& stringToSign, const std::string& key) const;
-    std::string GenerateStringToSign(const std::string& dateValue, const std::string& simpleDate, const std::string& canonicalRequestHash,
-                                const std::string& region, const std::string& serviceName) const;
-
-    std::string ComputeHash(const std::string& secretKey, const std::string& simpleDate, const std::string& region,
-                        const std::string& serviceName) const;
-    std::string ComputePayloadHash(HttpRequest& request) const;
-    DateTime GetSigningTimestamp() const { return DateTime::Now(); }
-
     Credential m_credential;
     std::string m_serviceName;
     std::string m_region;
-    std::set<std::string> m_unsignedHeaders;
-    std::unique_ptr<Sha256> m_hash;
-    std::unique_ptr<Sha256HMAC> m_hmac;
 };
 
 }
