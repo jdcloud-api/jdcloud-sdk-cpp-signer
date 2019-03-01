b8e30e847842ff173b8bcfc80df9c9589a0c08e3
LI Daobing <lidaobing@gmail.com>
2019-03-01 14:26:51 +0800

    add testcase(failed) for #7 and #50 (#51)
    * add SignRequest with uuid and timestamp
    
    * add test
    
    * fix compile problem under linux
    
    * it failed as i gussed
    
    * comment the unpassed testcase
    
    * comment unpassed test

--- src/tests/JdcloudSignerImplTest.cpp
+++ src/tests/JdcloudSignerImplTest.cpp
@@ -5,14 +5,19 @@
 using namespace jdcloud_signer;
 using namespace std;
 
-TEST(JdcloudSignerImpl, SignRequest) {
+HttpRequest BuildAndSignRequestFromUrl(const string& url) {
+    HttpRequest request(url, HttpMethod::HTTP_GET);
     Credential credential("ak", "sk");
     JdcloudSignerImpl signer(credential, "vm", "cn-north-1");
     DateTime now(INT64_C(1234567890000));
     string uuid("uuid");
-    HttpRequest request("http://vm.cn-north-1.jdcloud.net/", HttpMethod::HTTP_GET);
     bool result = signer.SignRequest(request, now, uuid);
-    ASSERT_TRUE(result);
+    assert(result);
+    return request;
+}
+
+TEST(JdcloudSignerImpl, SignRequest) {
+    auto request = BuildAndSignRequestFromUrl("http://vm.cn-north-1.jdcloud.net/");
     // TODO: #48
     // ASSERT_TRUE(request.HasHeader("User-Agent"));
     ASSERT_EQ(request.GetHeaders().size(), 4);
@@ -20,4 +25,16 @@ TEST(JdcloudSignerImpl, SignRequest) {
     ASSERT_EQ(request.GetHeaderValue("x-jdcloud-date"), "20090213T233130Z");
     ASSERT_EQ(request.GetHeaderValue("authorization"), "JDCLOUD2-HMAC-SHA256 Credential=ak/20090213/cn-north-1/vm/jdcloud2_request, SignedHeaders=host;x-jdcloud-date;x-jdcloud-nonce, Signature=43a76892222031f847c00c56cac8d4d2dc179a4e75a37868d09a5300435f4054");
     ASSERT_EQ(request.GetHeaderValue("host"), "vm.cn-north-1.jdcloud.net");
+}
+
+TEST(JdcloudSignerImpl, SignRequestWithEmptyQueryValue) {
+    auto auth1 = BuildAndSignRequestFromUrl("http://vm.cn-north-1.jdcloud.net/?a=&b=").GetHeaderValue("authorization");
+    // auto auth2 = BuildAndSignRequestFromUrl("http://vm.cn-north-1.jdcloud.net/?a&b=").GetHeaderValue("authorization");
+    // auto auth3 = BuildAndSignRequestFromUrl("http://vm.cn-north-1.jdcloud.net/?a=&b").GetHeaderValue("authorization");
+    // auto auth4 = BuildAndSignRequestFromUrl("http://vm.cn-north-1.jdcloud.net/?a&b").GetHeaderValue("authorization");
+    auto auth5 = BuildAndSignRequestFromUrl("http://vm.cn-north-1.jdcloud.net/?b=&a=").GetHeaderValue("authorization");
+    // EXPECT_EQ(auth1, auth2);
+    // EXPECT_EQ(auth1, auth3);
+    // EXPECT_EQ(auth1, auth4);
+    EXPECT_EQ(auth1, auth5);
 }
